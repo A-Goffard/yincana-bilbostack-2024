c1b6dfb477d790dd79a484b9a749d15e8a91e39a
A-Goffard <152596650+A-Goffard@users.noreply.github.com>
2024-01-20 20:23:30 +0100

    prueba correccion

--- scriptvideo.js
+++ scriptvideo.js
@@ -1,7 +1,6 @@
-//fondo matrix
 let canvas = document.querySelector("canvas");
 
-let ctx = canvas.getContext("2d"); 
+let ctx = canvas.getContext("2d");
 let width = canvas.width = window.innerWidth;
 let height = canvas.height = window.innerHeight;
 let str = "BILBOSTACK BILBOSTACK";
@@ -10,30 +9,39 @@ let font = 5;
 let col = width / font;
 let arr = [];
 
-for(let i = 0; i < col; i++) {
+for (let i = 0; i < col; i++) {
     arr[i] = 1;
 }
 
 const draw = () => {
     ctx.fillStyle = "rgba(0,0,0,0.05)";
     ctx.fillRect(0, 0, width, height);
-    ctx.fillStyle = "#ABDB2A"; 
-    ctx.font = "$(font)px system-iu";
+    ctx.fillStyle = "#ABDB2A";
+    ctx.font = "${font}px system-iu";
 
-    for(let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         let txt = matrix[Math.floor(Math.random() * matrix.length)];
         ctx.fillText(txt, i * font, arr[i] * font);
 
-        if(arr[i] * font > height && Math.random() > 0.975) {
-            arr[i] = 0;      
+        if (arr[i] * font > height && Math.random() > 0.975) {
+            arr[i] = 0;
         }
 
         arr[i]++;
     }
 };
 
+const handleResize = () => {
+    width = canvas.width = window.innerWidth;
+    height = canvas.height = window.innerHeight;
+    col = width / font;
+    arr = [];
+    for (let i = 0; i < col; i++) {
+        arr[i] = 1;
+    }
+    draw(); // Vuelve a dibujar el fondo matrix después de cambiar el tamaño del canvas
+};
 
 setInterval(draw, 20);
 
-window.addEventListener("resize", () => location.reload());
-
+window.addEventListener("resize", handleResize);
