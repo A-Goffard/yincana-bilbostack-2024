19d62d1bf86464a116fea0a7755ea417c2b4e0e9
A-Goffard <152596650+A-Goffard@users.noreply.github.com>
2024-01-20 20:28:20 +0100

    correccion tb en la pagina final

--- scriptvideoarcoiris.js
+++ scriptvideoarcoiris.js
@@ -1,6 +1,4 @@
-//fondo matrix
 let canvas = document.querySelector("canvas");
-
 let ctx = canvas.getContext("2d"); 
 let width = canvas.width = window.innerWidth;
 let height = canvas.height = window.innerHeight;
@@ -11,11 +9,11 @@ let col = width / font;
 let arr = [];
 const customColors = ["#E129FF", "#0019FF", "#00E0FF", "#21E800", "#E7D000", "#FF8A00", "#EE0000"];
 
-for(let i = 0; i < col; i++) {
+for (let i = 0; i < col; i++) {
     arr[i] = 1;
 }
 
-const getRondomColor = () => {
+const getRandomColor = () => {
     return customColors[Math.floor(Math.random() * customColors.length)];
 };
 
@@ -23,15 +21,15 @@ const draw = () => {
     ctx.fillStyle = "rgba(0,0,0,0.05)";
     ctx.fillRect(0, 0, width, height);
 
-
-    for(let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         let txt = matrix[Math.floor(Math.random() * matrix.length)];
-        ctx.fillText(txt, i * font, arr[i] * font);
-        ctx.fillStyle = getRondomColor();
-        ctx.font = `$(font)px system-iu`;
+        let color = getRandomColor();
+
+        ctx.fillStyle = color;
+        ctx.font = "${font}px system-iu";
         ctx.fillText(txt, i * font, arr[i] * font);
 
-        if(arr[i] * font > height && Math.random() > 0.975) {
+        if (arr[i] * font > height && Math.random() > 0.975) {
             arr[i] = 0;      
         }
 
@@ -39,7 +37,15 @@ const draw = () => {
     }
 };
 
-
 setInterval(draw, 20);
 
-window.addEventListener("resize", () => location.reload());
+window.addEventListener("resize", () => {
+    width = canvas.width = window.innerWidth;
+    height = canvas.height = window.innerHeight;
+    col = width / font;
+    arr = [];
+    for (let i = 0; i < col; i++) {
+        arr[i] = 1;
+    }
+    draw();
+});
